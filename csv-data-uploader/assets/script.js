5c587bd2c3374c2ffcf46075488aee4b1b55a4fc
Pratham Patel <prathampatel45201@gmail.com>
2025-02-25 00:28:14 +0530

    Plugin: download data as csv completed

--- csv-data-uploader/assets/script.js
+++ csv-data-uploader/assets/script.js
@@ -1,9 +1,7 @@
 jQuery(document).ready(function () {
   jQuery("#frm-csv-upload").on("submit", function (event) {
     event.preventDefault();
-
     var formData = new FormData(this);
-
     jQuery.ajax({
       url: cdu_object.ajax_url,
       data: formData,
---
+++ table-data-csv-backup/table-data-csv-backup.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Plugin Name: CSV Data backup
+ * Description: It will export table data into .csv file
+ * Version: 1.0
+ * Author: Pratham Patel
+ * Plugin URI: https://www.google.com
+ * Author URI: https://www.google.com
+ */
+
+// Admin menu
+add_action("admin_menu", "tdcb_create_admin_menu");
+
+// Admin Menu
+function tdcb_create_admin_menu()
+{
+    add_menu_page("CSV Data Backup Plugin", "CSV Data Backup", "manage_options", "csv-data-backup", "tdcb_export_form", "dashicons-database-export", 8);
+}
+
+// Form Layout
+function tdcb_export_form()
+{
+    ob_start();
+    include_once plugin_dir_path(__FILE__) . "/template/table_data_backup.php";
+    $layout = ob_get_contents();
+    ob_end_clean();
+    echo $layout;
+}
+add_action("admin_init", "tdcb_handle_form_export");
+
+function tdcb_handle_form_export()
+{
+    if (isset($_POST['tdcb_export_button'])) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . "students_data";
+        $students = $wpdb->get_results(
+            "SELECT * FROM {$table_name}",
+            ARRAY_A
+        );
+
+        if (empty($students)) {
+            // Error message
+        }
+
+        $filename = "students_data_" . time() . ".csv";
+        header("Content-Type: text/csv; charset=utf-8;");
+        header("Content-Disposition: attachment; filename=" . $filename);
+        $output = fopen("php://output", "w");
+        fputcsv($output, array_keys($students[0]));
+        foreach ($students as $student) {
+
+            fputcsv($output, $student);
+        }
+        fclose($output);
+        exit;
+    }
+}
